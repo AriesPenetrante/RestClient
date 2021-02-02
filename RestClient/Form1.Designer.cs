f670d4e3eadc9c0baeb0e561f0dcec31bb74d65b
Aries Penetrante <penetrantearies@gmail.com>
2021-02-02 12:15:41 +0200

    Add project files.

---
+++ RestClient/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace RestClientName
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.goBtn = new System.Windows.Forms.Button();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.copytofileBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // goBtn
+            // 
+            this.goBtn.Location = new System.Drawing.Point(15, 60);
+            this.goBtn.Name = "goBtn";
+            this.goBtn.Size = new System.Drawing.Size(75, 23);
+            this.goBtn.TabIndex = 0;
+            this.goBtn.Text = "GO!";
+            this.goBtn.UseVisualStyleBackColor = true;
+            this.goBtn.Click += new System.EventHandler(this.goBtn_Click);
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(15, 34);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(257, 20);
+            this.urlTextBox.TabIndex = 1;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(15, 102);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputTextBox.Size = new System.Drawing.Size(448, 171);
+            this.outputTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output";
+            // 
+            // copytofileBtn
+            // 
+            this.copytofileBtn.Location = new System.Drawing.Point(15, 291);
+            this.copytofileBtn.Name = "copytofileBtn";
+            this.copytofileBtn.Size = new System.Drawing.Size(75, 23);
+            this.copytofileBtn.TabIndex = 5;
+            this.copytofileBtn.Text = "Copy to File";
+            this.copytofileBtn.UseVisualStyleBackColor = true;
+            this.copytofileBtn.Click += new System.EventHandler(this.copytofileBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(369, 291);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(75, 23);
+            this.exitBtn.TabIndex = 6;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(224, 291);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 7;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 561);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.copytofileBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.urlTextBox);
+            this.Controls.Add(this.goBtn);
+            this.MaximumSize = new System.Drawing.Size(500, 600);
+            this.MinimumSize = new System.Drawing.Size(500, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "REST Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button goBtn;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button copytofileBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button clearBtn;
+    }
+}
+
---
+++ RestClient/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RestClientName
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+        private void Output(string strOutputText)
+        {
+            try
+            {
+                System.Diagnostics.Debug.Write(strOutputText + Environment.NewLine);
+                outputTextBox.Text = outputTextBox.Text + strOutputText + Environment.NewLine;
+                outputTextBox.SelectionStart = outputTextBox.TextLength;
+                outputTextBox.ScrollToCaret();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex.Message, ToString() + Environment.NewLine);
+            }
+        }
+
+        private void goBtn_Click(object sender, EventArgs e)
+        {
+
+            RESTClient rClient = new RESTClient();
+
+            rClient.endPoint = urlTextBox.Text;
+            Output("RESTClient Object created.");
+
+            string strJSON = string.Empty;
+
+            strJSON = rClient.makeRequest();
+
+            Output(strJSON);
+
+
+
+
+        }
+
+
+
+
+
+
+        private void writetofile()
+
+
+        {
+
+            ///Start of Writing 
+            ///make output file at C:\Output
+
+            string filePath = @"C:\Output\Output.txt";
+            string content = outputTextBox.Text;
+
+            using (StreamWriter outputFile = new StreamWriter(filePath))
+            {
+                outputFile.WriteLine(content);
+            }
+
+
+
+
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.ShowDialog();
+
+            string readfile = File.ReadAllText(dialog.FileName);
+
+            outputTextBox.Text = readfile;
+
+
+
+
+            ///end of writing
+
+
+
+
+        }
+
+        private void copytofileBtn_Click(object sender, EventArgs e)
+        {
+            writetofile();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.ExitThread();
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            outputTextBox.Text = string.Empty;
+        }
+    }
+
+}
